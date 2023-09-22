8e8d3e9c35878f53e4f9ac2b6216e8042385a6ce
nuthapon <nuthaponm79@gmail.com>
2023-09-22 18:14:41 +0700

    feat: config admin and model

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-from app.models import ImageDetection , ImageSummery
-# Register your models here.
 
-admin.site.register(ImageDetection)
-admin.site.register(ImageSummery)
+from app.models import ImageDetection , ImageSummery
+
+
+class ImageDetectionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'image', 'description', 'created_at', 'updated_at']
+    search_fields = ['id', 'image', 'description']
+
+
+class ImageSummeryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'image_detect', 'image_type', 'accuracy', 'created_at', 'updated_at']
+    search_fields = ['id', 'image_detect', 'image_type', 'accuracy']
+
+
+admin.site.register(ImageDetection, ImageDetectionAdmin)
+admin.site.register(ImageSummery, ImageSummeryAdmin)
--- app/models.py
+++ app/models.py
@@ -8,20 +8,34 @@ class ImageDetection(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f'Image id at {self.id}'
+    
+    class Meta:
+        verbose_name = 'ImageDetection'
+        verbose_name_plural = verbose_name
+
 
 class ImageSummery(models.Model):
-    image_type_choices = ((0, 'HmeeKhor (หมี่ขอ)'),
-                          (1, 'Heart (หัวใจ)'),
-                          (2, 'HmorNam (หม้อน้ำ)'),
-                          (3, 'HmeeRuad (หมี่รวด)'),
-                          (4, 'BrokenThread (เส้นยืนขาด)'),
-                          (5, 'HitchSilk (ขี้ไหม)'),
-                          (6, 'SlackThread (เส้นยืนหย่อน)'))
+    image_type_choices = (
+        (0, 'HmeeKhor (หมี่ขอ)'),
+        (1, 'Heart (หัวใจ)'),
+        (2, 'HmorNam (หม้อน้ำ)'),
+        (3, 'HmeeRuad (หมี่รวด)'),
+        (4, 'BrokenThread (เส้นยืนขาด)'),
+        (5, 'HitchSilk (ขี้ไหม)'),
+        (6, 'SlackThread (เส้นยืนหย่อน)')
+    )
 
     image_detect = models.ForeignKey(ImageDetection, on_delete=models.CASCADE)
-    image_type = models.IntegerField(
-        choices=image_type_choices, null=False, blank=False)
-    accuracy = models.FloatField(
-        null=False, blank=True, help_text='cof in yolo')
+    image_type = models.IntegerField(choices=image_type_choices, null=False, blank=False)
+    accuracy = models.FloatField(null=False, blank=True, help_text='cof in yolo')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Image Summary at {self.id} result={self.get_image_type_display()} accuracy={self.accuracy}'
+    
+    class Meta:
+        verbose_name = 'ImageSummery'
+        verbose_name_plural = verbose_name
