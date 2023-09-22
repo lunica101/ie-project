17cd229a8558a660f56a98e6c3535726542c7cab
lunica101 <129298409+lunica101@users.noreply.github.com>
2023-09-22 18:24:11 +0700

    Update detect.py

--- app/views/detect.py
+++ app/views/detect.py
@@ -4,11 +4,5 @@ from django.contrib.auth.models import User
 
 
 def detect_page(request):
-    if request.method == 'POST':
-        image = request.POST.get('image',None)
-        description = request.POST.get('description',None)
-        print(image)
-        print(description)
-        return render(request , 'pages/showresult.html')
     return render(request , 'pages/detection.html')
  
